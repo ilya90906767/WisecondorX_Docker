852dcc80f11cc8cc48cfb67fee45aecf73390cb8
Ilya Betyaev <110731177+ilya90906767@users.noreply.github.com>
2024-04-26 18:24:42 +0300

    Add files via upload

---
+++ wisecondorx.sh
@@ -0,0 +1,82 @@
+docker build -t wisecondorx:latest . 
+
+CONTAINER="wisecondor:latest"
+operation=$1 
+
+if [ "$operation" = "convert"]; then 
+
+    bam_folder=$2
+    output_folder=$3
+    threads=$4
+
+    mkdir -p "$output_folder"
+
+    bam_files=($(find "$bam_folder" -type f -name "*.bam"))
+    echo "Converting BAM files to NPZ using WisecondorX..."
+    for bam_file in "${bam_files[@]}"; do
+        output_file="${output_folder}/${bam_file%.bam}.npz"
+        echo "Converting $bam_file to $output_file"
+        echo "docker run --rm -v "$bam_folder":/input -v "$output_folder":/output $CONTAINER WisecondorX convert /input/"$bam_file" /output/"$output_file""  >> WiseConvert.logs
+        docker run --rm -v "$bam_folder":/input -v "$output_folder":/output $CONTAINER WisecondorX convert /input/"$bam_file" /output/"$output_file" &
+    done | xargs -P "$threads" -n 1
+
+   wait
+   echo "All BAM files converted to NPZ successfully!" >> WiseConvert.logs
+  echo "Conversion complete."
+
+elif [ "$operation" = "newref"]; then 
+
+    input_folder=$2
+    output_file=$3
+    threads=$4
+    docker run --rm -v "$input_folder":/input -v "$(dirname "$output_file")":/output $CONTAINER WisecondorX newref /input/*.npz /output/"$output_file" --cpus $threads
+    echo "All NPZ files processed successfully using WisecondorX newref!"
+
+elif [ "$operation" = "predict"]; then 
+
+    input_folder=$2
+    reference=$3 
+    output_folder=$4
+    threads=$5
+    
+    npz_to_predict=($(find "$input_folder" -type f -name "*.npz"))
+    echo "Predicting NPZ files using WisecondorX..."
+    for npz_file in "${npz_to_predict[@]}"; do 
+        output_id="${output_folder}/${npz_file%.npz}"
+        echo "Predicting $output_id"
+        echo "docker run --rm -v "$input_folder":/input -v "$reference":/reference -v "$output_folder":/output $CONTAINER WisecondorX predict /input/"$npz_file" /reference/"$reference" /output/"$output_id" --plot --bed" >> WisePredict.logs
+        docker run --rm -v "$input_folder":/input -v "$reference":/reference -v "$output_folder":/output $CONTAINER WisecondorX predict /input/"$npz_file" /reference/"$reference" /output/"$output_id" --plot --bed &
+    done | xargs -P "$threads" -n 1
+
+    wait 
+    echo "All NPZ files were predicted. You can check results in $output_folder"
+    echo "Prediction complete"
+
+else
+  echo "Invalid operation. Please use 'convert' as the first argument."
+fi
+
+    # INPUT_BAM_DIR="$1"
+    # THREADS=$2
+
+    # if [ $# -ne 2]; then 
+    #     echo "Usage: $0 [path_to_bam_files] [threads]"
+    #     exit 1 
+    # fi 
+
+    # if [ ! -d "$INPUT_BAM_DIR" ]; then 
+    #     echo "Directory does not exist: $INPUT_BAM_DIR"
+    #     exit 1 
+    # fi 
+
+    # docker_run_function() {
+    #     bamfile=$1
+    #     echo "Processing BAM file: $bamfile"
+    #     docker run --rm -v "$INPUT_BAM_DIR":/data "$CONTAINER" sh -c "samtools view -q 30 /data/$bamfile | python2 /home/karyo_RxRy_script.py --chr21"
+    # }
+
+    # export -f docker_run_function
+
+    # rm -f results.txt 
+
+    # find "$INPUT_BAM_DIR" -type f -name "*.bam" -print0 | xargs -O -n 1 -P $THREADS -I {} bash -c 'docker_run_function "{}" >> results.txt' _
